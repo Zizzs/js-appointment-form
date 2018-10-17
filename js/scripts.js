29c350ce1f96d7cf70214df0eb1c6bc82d853b7a
Alex Williams and Tanner Damron <tanner@>
2018-10-17 15:01:39 -0700

    create forms that submit inputs and return inputs in modal

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+
+  $("form#makeAppointment").submit(function(event) {
+    var nameTemp = $("input#nameInput").val();
+    $(".modalName").text(nameTemp);
+    event.preventDefault();
+  });
+
+  $("form#makeAppointment").submit(function(event) {
+    var descriptionTemp = $("input#descriptionInput").val();
+    $(".modalDescription").text(descriptionTemp);
+    event.preventDefault();
+  });
+
+  $("form#makeAppointment").submit(function(event) {
+    var dateTemp = $("input#dateInput").val();
+    $(".modalDate").text(dateTemp);
+    event.preventDefault();
+  });
+
+  $("form#makeAppointment").submit(function(event) {
+    var timesTemp = $("input#timesInput").val();
+    $(".modalTimes").text(timesTemp);
+    event.preventDefault();
+  });
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+  }}
+
+});
